038a45cc353f8908209c081ded9b86a1668df3bd
Bryan Lara <bralavar@gmail.com>
2023-07-23 17:24:32 -0500

    chore: configure public axios intance

---
+++ apps/nafasi/src/service/public.js
@@ -0,0 +1,56 @@
+import mem from "mem";
+import axios from "axios";
+
+const baseURL = "http://sebratech.recuirt.com:8000/api";
+
+const axiosPublic = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const logut = () => localStorage.removeItem("session");
+
+const login = async (params) => {
+  try {
+    const { data: session } = await axiosPublic.post("/token/", params);
+
+    localStorage.setItem("session", JSON.stringify(session));
+
+    return session;
+  } catch (_) {
+    logut();
+
+    return null;
+  }
+};
+
+const refreshTokenUnauth = async () => {
+  const session = JSON.parse(localStorage.getItem("session"));
+
+  try {
+    const { data } = await axiosPublic.post("/token/refresh/", {
+      refresh: session?.refresh || "",
+    });
+
+    logut();
+
+    localStorage.setItem(
+      "session",
+      JSON.stringify({ refresh: session?.refresh || "", access: data.access })
+    );
+
+    return data.access;
+  } catch (_) {
+    logut();
+
+    return "";
+  }
+};
+
+const refreshToken = mem(refreshTokenUnauth, {
+  maxAge: 10000,
+});
+
+export { refreshToken, login, baseURL };
