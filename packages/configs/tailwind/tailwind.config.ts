f781085a83607dfa7d72809a776156b7d9940bd8
seballos <sebas11cs@gmail.com>
2024-01-03 11:18:45 -0500

    chore(tailwind): share tailwind config with nafasi app

--- packages/configs/tailwind/tailwind.config.ts
+++ packages/configs/tailwind/tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "../../packages/ui/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
